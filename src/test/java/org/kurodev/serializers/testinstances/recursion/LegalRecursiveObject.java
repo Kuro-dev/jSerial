0efbb053e21d36ffcd88cff2f399938d482a0556
Kuro <dev_kuro@yahoo.de>
2021-05-25 20:44:46 +0200

    Implemented basic recursion for object serialization

---
+++ src/test/java/org/kurodev/serializers/testinstances/recursion/LegalRecursiveObject.java
@@ -0,0 +1,13 @@
+package org.kurodev.serializers.testinstances.recursion;
+
+public class LegalRecursiveObject {
+    RecursiveObject4 obj1 = new RecursiveObject4();
+    RecursiveObject4 obj2 = new RecursiveObject4();
+    RecursiveObject4 obj3 = new RecursiveObject4();
+    RecursiveObject4 obj4 = new RecursiveObject4();
+    RecursiveObject4 obj5 = new RecursiveObject4();
+    RecursiveObject4 obj6 = new RecursiveObject4();
+    RecursiveObject4 obj7 = new RecursiveObject4();
+    RecursiveObject4 obj8 = new RecursiveObject4();
+    RecursiveObject4 obj9 = new RecursiveObject4();
+}
---
+++ src/test/java/org/kurodev/serializers/testinstances/recursion/RecursiveObject3.java
@@ -0,0 +1,6 @@
+package org.kurodev.serializers.testinstances.recursion;
+
+public class RecursiveObject3 {
+    int c = 5;
+    RecursiveObject4 obj = new RecursiveObject4();
+}
